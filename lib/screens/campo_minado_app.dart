091c7fc0ee6f53a728ee10d50c29965d8d174116
William Azevedo <willianray@gmail.com>
2020-02-18 14:02:14 -0300

    Adicionando classes do campo minado

---
+++ lib/screens/campo_minado_app.dart
@@ -0,0 +1,85 @@
+import 'package:campo_minado/components/resultado_widget.dart';
+import 'package:campo_minado/components/tabuleiro_widget.dart';
+import 'package:campo_minado/models/campo.dart';
+import 'package:campo_minado/models/explosao_exception.dart';
+import 'package:campo_minado/models/tabuleiro.dart';
+import 'package:flutter/material.dart';
+
+class CampoMinadoApp extends StatefulWidget {
+  @override
+  _CampoMinadoAppState createState() => _CampoMinadoAppState();
+}
+
+class _CampoMinadoAppState extends State<CampoMinadoApp> {
+  bool _venceu;
+  Tabuleiro _tabuleiro;
+
+  Tabuleiro _getTabuleiro(double largura, double altura) {
+    if (_tabuleiro == null) {
+      var qtdeColunas = 15;
+      var tamanhoCampo = largura / qtdeColunas;
+      var qtdeLinhas = (altura / tamanhoCampo).floor();
+      _tabuleiro =
+          Tabuleiro(linhas: qtdeLinhas, colunas: qtdeColunas, qtdeBombas: 50);
+    }
+    return _tabuleiro;
+  }
+
+  _reinciar() {
+    setState(() {
+      _venceu = null;
+      _tabuleiro.reiniciar();
+    });
+  }
+
+  void _abrir(Campo campo) {
+    if (_venceu != null) {
+      return;
+    }
+    setState(() {
+      try {
+        campo.abrir();
+        if (_tabuleiro.resolvido) {
+          _venceu = true;
+        }
+      } on ExplosaoException {
+        _venceu = false;
+        _tabuleiro.revelarBombas();
+      }
+    });
+  }
+
+  void _alternarMarcacao(Campo campo) {
+    if (_venceu != null) {
+      return;
+    }
+    setState(() {
+      campo.alternarMarcacao();
+      if (_tabuleiro.resolvido) {
+        _venceu = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: ResultadoWidget(
+          venceu: _venceu,
+          onReiniciar: _reinciar,
+        ),
+        body: Container(child: LayoutBuilder(
+          builder: (ctx, constraints) {
+            return TabuleiroWidget(
+              tabuleiro:
+                  _getTabuleiro(constraints.maxWidth, constraints.maxHeight),
+              onAbrir: _abrir,
+              onAlternarMarcacao: _alternarMarcacao,
+            );
+          },
+        )),
+      ),
+    );
+  }
+}
---
+++ test/models/campo_test.dart
@@ -0,0 +1,58 @@
+import 'package:campo_minado/models/campo.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+  group('Campo', () {
+    test('Abrir Campo COM Explosão', () {
+      var c = Campo(linha: 0, coluna: 0);
+      c.minar();
+
+      expect(c.abrir, throwsException);
+    });
+
+    test('Abrir Campo SEM Explosão', () {
+      var c = Campo(linha: 0, coluna: 0);
+      c.abrir();
+      expect(c.aberto, isTrue);
+    });
+
+    test('Adicionar NÃO Vizinho', () {
+      var c1 = Campo(linha: 0, coluna: 0);
+      var c2 = Campo(linha: 1, coluna: 3);
+
+      c1.adicionarVizinho(c2);
+      expect(c1.vizinhos.isEmpty, isTrue);
+    });
+
+    test('Adicionar Vizinho', () {
+      var c1 = Campo(linha: 3, coluna: 3);
+      var c2 = Campo(linha: 3, coluna: 4);
+      var c3 = Campo(linha: 2, coluna: 2);
+      var c4 = Campo(linha: 4, coluna: 4);
+
+      c1.adicionarVizinho(c2);
+      c1.adicionarVizinho(c3);
+      c1.adicionarVizinho(c4);
+      
+      expect(c1.vizinhos.length, 3);
+    });
+
+    test('Minas na Vizinhança', () {
+      var c1 = Campo(linha: 3, coluna: 3);
+
+      var c2 = Campo(linha: 3, coluna: 4);
+      c2.minar();
+
+      var c3 = Campo(linha: 2, coluna: 2);
+
+      var c4 = Campo(linha: 4, coluna: 4);
+      c4.minar();
+
+      c1.adicionarVizinho(c2);
+      c1.adicionarVizinho(c3);
+      c1.adicionarVizinho(c4);
+
+      expect(c1.qtdeMinasNaVizinhanca, 2);
+    });
+  });
+}
